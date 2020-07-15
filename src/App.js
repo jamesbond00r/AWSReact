3398de4065b623c5ae97bdd2c26db1e245575c4a
Owner <jamesbond00r@gmail>
2020-07-14 20:38:29 -0500

    add react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          We changed parts here!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Nav variant="tabs" defaultActiveKey="/home">
+  <Nav.Item>
+    <Nav.Link href="/home">Home</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-1">Lucky</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-2">Tips</Nav.Link>
+  </Nav.Item>
+</Nav>
+        
+
+  <Jumbotron>
+  <h1>Hello, world!</h1>
+  <p>
+    I made a button and a Jumbotron and a nav bar.
+  </p>
+  <p>
+    <Button variant="danger">A Button</Button>
+  </p>
+</Jumbotron>
+        
     </div>
   );
 }
