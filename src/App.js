493c7ce7b1a256c341a63487d9f5d4afd7ea4ca6
Owner <jamesbond00r@gmail>
2020-07-15 20:05:16 -0500

    start modularizing

--- src/App.js
+++ src/App.js
@@ -2,34 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
-import Nav from 'react-bootstrap/Nav';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Bar from './nav.js';
+
 
 function App() {
   return (
     <div className="App">
-   <Nav variant="tabs" defaultActiveKey="/home">
-  <Nav.Item>
-    <Nav.Link href="/home">Home</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="link-1">Lucky</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="link-2">Tips</Nav.Link>
-  </Nav.Item>
-</Nav>
-        
+   <Bar></Bar>
 
-  <Jumbotron>
+  <Jumbotron variant="success">
+  
   <h1>Hello, world!</h1>
-  <p>
+  <p >
     I made a button and a Jumbotron and a nav bar.
   </p>
   <p>
     <Button variant="danger">A Button</Button>
   </p>
+  
 </Jumbotron>
         
     </div>
---
+++ src/nav.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+
+
+export default function Bar () {
+
+	return(
+
+
+<Nav className="justify-content-end" variant="tabs" defaultActiveKey="/home">
+  <Nav.Item>
+    <Nav.Link href="/home">Home</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-1">Lucky</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-2">Tips</Nav.Link>
+  </Nav.Item>
+</Nav>
+        
+)
+        }
