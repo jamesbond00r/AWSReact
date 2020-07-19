69022d1f5453a63f2618b8bc4f494da3e5194f5e
Owner <jamesbond00r@gmail>
2020-07-19 16:44:23 -0500

    add nav bar to contest

--- src/Contest.js
+++ src/Contest.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import NavBarLower from './NavBarLower';
 
 class Contest extends React.Component {
 	render() {
@@ -12,11 +12,13 @@ class Contest extends React.Component {
  <a href="https://www.instructables.com/contest/frozentreats2020/">Instructables Forzen Treats </a>
  <p>Make those hot summer days more enjoyable with a frozen treat! Share your favorite recipes for frozen treats for a chance to win an Amazon Gift Card! </p>
   <a href="https://www.instructables.com/contest/frozentreats2020/"> <Button variant="info">Contest Rules</Button></a>
+    
+ <NavBarLower></NavBarLower>
+
     </div>
     );
   }
 }
-
 
 
 
