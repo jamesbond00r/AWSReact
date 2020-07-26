74b6e70ea863eed20515b434523d784903d98e42
Owner <jamesbond00r@gmail>
2020-07-25 20:33:24 -0500

    add meta tages for SEO and h1 tag for SEO

--- src/lucky.js
+++ src/lucky.js
@@ -10,8 +10,9 @@ import Mailchimp from 'react-mailchimp-form';
 class Lucky extends React.Component {
 	render() {
 		return (
-      <div className="lucky">
-      <p>Sign up for lucky emails and get new contest and sweepstakes sent to your email!</p>
+      
+      <div className="lucky"><h1>Sign up for lucky emails</h1>
+      <p>Sign up for lucky emails and get new contest and sweepstakes sent to your email! Cancel emamil updates anytime</p>
  <Mailchimp
         action="https://protonmail.us10.list-manage.com/subscribe/post?u=901a183ea4387919f46d5e0b4&amp;id=46fa43a086"
         fields={[
